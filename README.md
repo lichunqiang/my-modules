18909f436a8aea99cec3724a9dd6ef99f081e638
lichunqiang <light-li@hotmail.com>
2014-11-28 13:51:35 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## 说明
+
+这里存放自己封装的改造的seajs模块
+
+## 缘起
+
+https://github.com/cmdjs/gallery/blob/master/README.md
+
+## 更多
+
+Coming soon
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var shell = require('shelljs/global');
+var fs = require('fs');
+
+
+
+/***************执行计划，打包spm包************************/
+var target_dir = 'js',
+	idleading = 'gallery/{{name}}/{{version}}';
+
+var spm_modules = [
+	'form/1.0.0',
+	'jquery-cookie/1.4.1',
+	'jquery/2.1.1',
+	'jquery/1.8.3',
+];
+
+var my_modules =  [
+	'jquery-validation/1.13.1'
+];
+
+gulp.task('build-gallery', function(cb){
+	spm_modules.forEach(function(_module){
+		//检查文件是否已经生成，如果存在略过
+		fs.exists(target_dir + '/gallery/' + _module, function(exists){
+			if(!exists) {
+				exec('spm build --output-directory ' + target_dir + ' --input-directory spm_modules/' + _module + ' --idleading ' + idleading);
+			}
+		});
+	});
+	my_modules.forEach(function(_module){
+		fs.exists(target_dir + '/gallery/' + _module, function(exists){
+			if(!exists) {
+				exec('spm build --output-directory ' + target_dir + ' --input-directory my_modules/' + _module + ' --idleading ' + idleading);
+			}
+		});
+	});
+});
